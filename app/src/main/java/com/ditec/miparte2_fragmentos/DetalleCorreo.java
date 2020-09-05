7bb67bca1e70eae2d092baa11fc4a30548aaa686
Wilson Callisaya <demarktec@gmail.com>
2020-09-04 22:20:07 -0500

    Ejemplo2 usando un recyclerview con un pojo de datos, y enviando parametros desde el adaptador a una actividad

---
+++ app/src/main/java/com/ditec/miparte2_fragmentos/DetalleCorreo.java
@@ -0,0 +1,20 @@
+package com.ditec.miparte2_fragmentos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DetalleCorreo extends AppCompatActivity {
+
+    TextView txtmidetalle;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle_correo);
+        txtmidetalle=findViewById(R.id.TxtDetalle);
+        txtmidetalle.setText("DE: "+ getIntent().getExtras().getString("de")+
+                " ASUNTO: "+ getIntent().getExtras().getString("asunto")+
+                " CONTENIDO: "+ getIntent().getExtras().getString("text"));
+    }
+}
